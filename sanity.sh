5327af5f9bed1c7c47c6c439d26ee51657bd91f9
Mukul Mehta <mukul.csiitkgp@gmail.com>
2020-07-06 23:49:33 +0530

    Exclude docs folder from sanity checks

--- sanity.sh
+++ sanity.sh
@@ -1,4 +1,4 @@
 dodgy --ignore paths venv env migrations scripts
-isort -rc --atomic ./ -sg "*/migrations/*.py"
+isort -rc --atomic ./ -sg "*/migrations/*.py" -sg "*/docs/*.py"
 pydocstyle --config=./.pydocstylerc
 pycodestyle ./ --config=./.pycodestylerc
